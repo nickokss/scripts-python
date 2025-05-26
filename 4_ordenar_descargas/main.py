5c6d429cb8fa7f2cc3cba8cd69e1f7db16ac9163
Nicolás Cortizo <ncsalgueiro92@gmail.com>
2025-05-26 13:43:26 +0200

    Update main.py

--- 4_ordenar_descargas/main.py
+++ 4_ordenar_descargas/main.py
@@ -2,32 +2,35 @@ import os
 import shutil
 
 def organizar_descargas():
-  carpeta_descargas = os.path.join(os.path.expanduser("~", "Downloads")
-  subcarpetas = {
-      "Documentos": [".pdf", ".docs", ".xlsx",".xls", ".txt"]
-      "Imagenes": [".jpg", ".jpeg"]
-  
-  }
+    carpeta_descargas = os.path.join(os.path.expanduser("~", "Downloads")
+    subcarpetas = {
+        "Documentos": [".pdf", ".docs", ".xlsx", ".xls", ".txt", ".pptx"],
+        "Imagenes": [".jpg", ".jpeg", ".png", ".gif"],
+        "Videos": [".mp4", ".mov", ".avi"],
+        "Programas": [".exe", ".msi"],
+        "Comprimidos": [".zip", ".rar",".7z"],
+        "Otros":[]
+    }
 
-for archivo in os.listdir(carpeta_descargas):
-    ruta_archivo = os.path.join(carpeta_descargas, archivo)
+    for archivo in os.listdir(carpeta_descargas):
+        ruta_archivo = os.path.join(carpeta_descargas, archivo)
 
-    if os.path.isfile(ruta_archivo):
-        extension = os.path.splitext(archivo)[1].lower()
-        movido = False
+        if os.path.isfile(ruta_archivo):
+            extension = os.path.splitext(archivo)[1].lower()
+            movido = False
 
-        for subcarpeta, extensiones in subcarpetas.items():
-            if extension in extensiones:
-                carpeta_destino = os.path.join(carpeta_descargas, subcarpeta)
+            for subcarpeta, extensiones in subcarpetas.items():
+                if extension in extensiones:
+                    carpeta_destino = os.path.join(carpeta_descargas, subcarpeta)
+                    os.makedirs(carpeta_destino, exist_ok=True)
+                    shutil.move(ruta_archivo, os.path.join(carpeta_destino, archivo))
+                    movido = True
+                    break
+                  
+            if not movido:
+                carpeta_destino = os.path.join(carpeta_descargas, "Otros")
                 os.makedirs(carpeta_destino, exist_ok=True)
                 shutil.move(ruta_archivo, os.path.join(carpeta_destino, archivo))
-                movido = True
-                break
-              
-        if not movido:
-            carpeta_destino = os.path.join(carpeta_descargas, "Otros")
-            os.makedirs(carpeta_destino, exist_ok=True)
-            shutil.move(ruta_archivo, os.path.join(carpeta_destino, archivo))
 
 if __name__ == "__main__":
     organizar_descargas()
